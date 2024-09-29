7f80c91617492ca56c0bdddb7f1fa817c930778a
anjusings <anjusings777@gmail.com>
2024-09-29 00:13:12 -0400

    Add basic streamlit layout

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title ="Multipage App",
+    
+
+)
+
+st.header("What is Skin Cancer?")
+st.markdown("Skin cancer is a type of cancer that begins in the cells of the skin. It occurs when the skin cells start to grow uncontrollably due to DNA damage, often caused by exposure to ultraviolet (UV) radiation from the sun or tanning beds. There are several types of skin cancer, but the most common ones are basal cell carcinoma (BCC), squamous cell carcinoma (SCC), and melanoma.")
